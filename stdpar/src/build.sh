336c4dcfc811f504c085316f39e32035c62d16ce
sdp <sdp@a4bf0192d1ed.jf.intel.com>
2023-05-12 04:06:27 -0700

    Change build.sh

--- stdpar/src/build.sh
+++ stdpar/src/build.sh
@@ -11,21 +11,29 @@ then
     exit 1
 fi
 
+FLAGS=""
+
+if [ "$DEBUG" == "ON" ]
+then
+    echo "Debug mode"
+    FLAGS+="-DSTDPAR_DEBUG"
+fi
+
 echo "building lulesh"
 
 if [ "$STDPAR" == "ON" ]
 then
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DSTDPAR -DUSE_USM_VECTOR -I /tmp/kboyarin/LULESH/generated_headers -I /tmp/kboyarin/oneDPL/include -o lulesh.o lulesh.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I /tmp/kboyarin/oneDPL/include -o lulesh-comm.o lulesh-comm.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I /tmp/kboyarin/oneDPL/include -o lulesh-viz.o lulesh-viz.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I /tmp/kboyarin/oneDPL/include -o lulesh-util.o lulesh-util.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I /tmp/kboyarin/oneDPL/include -o lulesh-init.o lulesh-init.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DSTDPAR -DUSE_USM_VECTOR $FLAGS -I /tmp/kboyarin/LULESH/generated_headers -I /tmp/kboyarin/oneDPL/include -o lulesh.o lulesh.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-comm.o lulesh-comm.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-viz.o lulesh-viz.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-util.o lulesh-util.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-init.o lulesh-init.cc
 else
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I /tmp/kboyarin/oneDPL/include -o lulesh.o lulesh.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I /tmp/kboyarin/oneDPL/include -o lulesh-comm.o lulesh-comm.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I /tmp/kboyarin/oneDPL/include -o lulesh-viz.o lulesh-viz.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I /tmp/kboyarin/oneDPL/include -o lulesh-util.o lulesh-util.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I /tmp/kboyarin/oneDPL/include -o lulesh-init.o lulesh-init.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh.o lulesh.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-comm.o lulesh-comm.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-viz.o lulesh-viz.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-util.o lulesh-util.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 $FLAGS -I /tmp/kboyarin/oneDPL/include -o lulesh-init.o lulesh-init.cc
 fi
 
 echo "linking lulesh"
