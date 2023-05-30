8dc3ace1e0b97344fc8a41295ffa6c16353158eb
kboyarinov <konstantin.boyarinov@intel.com>
2023-05-30 15:33:32 +0300

    Rewrite build.sh

--- stdpar/src/build.sh
+++ stdpar/src/build.sh
@@ -4,7 +4,7 @@ STDPAR="$1"
 
 if [ -z "$1" ]
 then
-    echo "STDPAR not specified - ON by default"
+    echo "GPU STDPAR not specified - ON by default"
     STDPAR="ON"
 fi
 
@@ -19,21 +19,24 @@ fi
 GENERATED_HEADERS="/tmp/kboyarin/LULESH/generated_headers"
 DPL_INCLUDE="/tmp/kboyarin/oneDPL/include"
 
-CXX_FLAGS=""
-
-echo "building lulesh: STDPAR = $STDPAR LULESH_STDPAR_POLICY = $LULESH_STDPAR_POLICY"
+CXX_FLAGS="-DLULESH_STDPAR_POLICY=$LULESH_STDPAR_POLICY"
+LINK_FLAGS=""
 
 if [ "$STDPAR" == "ON" ]
 then
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DSTDPAR -DLULESH_STDPAR_POLICY=$LULESH_STDPAR_POLICY -I $GENERATED_HEADERS -I $DPL_INCLUDE -o lulesh.o lulesh.cc
-else
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh.o lulesh.cc
+    CXX_FLAGS+="-DSTDPAR"
+    LINK_FLAGS+= "-loverusm"
 fi
 
+
+
+echo "building lulesh: GPU STDPAR = $STDPAR LULESH_STDPAR_POLICY = $LULESH_STDPAR_POLICY"
+
+icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI $CXX_FLAGS -I $DPL_INCLUDE -o lulesh.o lulesh.cc
 icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-comm.o lulesh-comm.cc
 icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-viz.o lulesh-viz.cc
 icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-util.o lulesh-util.cc
 icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-init.o lulesh-init.cc
-
+s
 echo "linking lulesh"
-icpx -fsycl -O3 -w -std=c++17 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -o lulesh -ltbb -loverusm
+icpx -fsycl -O3 -w -std=c++17 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -o lulesh -ltbb $LINK_FLAGS
