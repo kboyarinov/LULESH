f620ba7e1a8a6ee932aee29d4dde4551579ae666
kboyarinov <konstantin.boyarinov@intel.com>
2023-05-30 15:12:39 +0300

    Change build.sh

--- stdpar/src/build.sh
+++ stdpar/src/build.sh
@@ -11,18 +11,18 @@ then
     exit 1
 fi
 
-GENERATED_HEADERS="/home/sdp/kboyarin/LULESH/generated_headers"
-DPL_INCLUDE="/home/sdp/kboyarin/oneDPL/include"
+GENERATED_HEADERS="/tmp/kboyarin/LULESH/generated_headers"
+DPL_INCLUDE="/tmp/kboyarin/oneDPL/include"
 
 echo "building lulesh"
 
 if [ "$STDPAR" == "ON" ]
 then
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DSTDPAR -DUSE_USM_VECTOR -I $GENERATED_HEADERS -I $DPL_INCLUDE -o lulesh.o lulesh.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I $DPL_INCLUDE -o lulesh-comm.o lulesh-comm.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I $DPL_INCLUDE -o lulesh-viz.o lulesh-viz.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I $DPL_INCLUDE -o lulesh-util.o lulesh-util.cc
-    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DUSE_USM_VECTOR -I $DPL_INCLUDE -o lulesh-init.o lulesh-init.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -DSTDPAR -I $GENERATED_HEADERS -I $DPL_INCLUDE -o lulesh.o lulesh.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-comm.o lulesh-comm.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-viz.o lulesh-viz.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-util.o lulesh-util.cc
+    icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-init.o lulesh-init.cc
 else
     icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh.o lulesh.cc
     icpx -fsycl -O3 -c -w -std=c++17 -DUSE_MPI=0 -I $DPL_INCLUDE -o lulesh-comm.o lulesh-comm.cc
@@ -32,4 +32,4 @@ else
 fi
 
 echo "linking lulesh"
-icpx -fsycl -O3 -w -std=c++17 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -o lulesh -ltbb
+icpx -fsycl -O3 -w -std=c++17 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -o lulesh -ltbb -loverusm
