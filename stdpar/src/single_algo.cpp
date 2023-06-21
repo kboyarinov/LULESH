3b33e7bb97f2d796fb5ed9f57c1f839467c6a67b
kboyarinov <konstantin.boyarinov@intel.com>
2023-06-21 14:01:14 +0300

    Single algo change

--- stdpar/src/single_algo.cpp
+++ stdpar/src/single_algo.cpp
@@ -7,33 +7,90 @@
 #include <execution>
 #include <sycl/sycl.hpp>
 
-int main() {
-    std::mt19937 random_generator(42);
-    std::size_t n = 50000000;
-    std::size_t scalar = 42;
+using T = float;
+std::size_t n = 50000000;
+std::mt19937 random_generator(42);
 
-    using T = float;
+T* allocate(std::size_t count) {
+    T* ptr =  reinterpret_cast<T*>(sycl::malloc_shared(count * sizeof(T), oneapi::dpl::execution::dpcpp_default.queue()));
+    for (std::size_t i = 0; i < count; ++i) {
+        ::new(ptr + i) T(random_generator());
+    }
+    return ptr;
+}
 
-    T* data_from1 = reinterpret_cast<T*>(sycl::malloc_shared(n * sizeof(T), oneapi::dpl::execution::dpcpp_default.queue()));
-    T* data_from2 = reinterpret_cast<T*>(sycl::malloc_shared(n * sizeof(T), oneapi::dpl::execution::dpcpp_default.queue()));
-    T* data_to = reinterpret_cast<T*>(sycl::malloc_shared(n * sizeof(T), oneapi::dpl::execution::dpcpp_default.queue()));
+void free(T* ptr, std::size_t count) {
+    for (std::size_t i = 0; i < count; ++i) {
+        (ptr + i)->~T();
+    }
+    sycl::free(ptr, oneapi::dpl::execution::dpcpp_default.queue());
+}
 
-    for (std::size_t i = 0; i < n; ++i) {
-        ::new(data_from1 + i) T(random_generator());
-        ::new(data_from2 + i) T(random_generator());
-        ::new(data_to + i) T(0);
+class Domain {
+public:
+    Domain()
+        : m_e(allocate(n))
+        , m_delv(allocate(n))
+        , m_p(allocate(n))
+        , m_q(allocate(n))
+        , m_qq(allocate(n))
+        , m_ql(allocate(n))
+    {}
+
+    ~Domain() {
+        free(m_e, n);
+        free(m_delv, n);
+        free(m_p, n);
+        free(m_q, n);
+        free(m_qq, n);
+        free(m_ql, n);
     }
 
+    T& e(std::size_t index) { return m_e[index]; }
+    T& delv(std::size_t index) { return m_delv[index]; }
+    T& p(std::size_t index) { return m_p[index]; }
+    T& q(std::size_t index) { return m_q[index]; }
+    T& qq(std::size_t index) { return m_qq[index]; }
+    T& ql(std::size_t index) { return m_ql[index]; }
+
+private:
+    T* m_e;
+    T* m_delv;
+    T* m_p;
+    T* m_q;
+    T* m_qq;
+    T* m_ql;
+};
+
+int main() {
     std::vector<double> times;
     std::size_t n_times = 11;
     times.reserve(n_times);
+
+    T* e_old = allocate(n);
+    T* delvc = allocate(n);
+    T* p_old = allocate(n);
+    T* q_old = allocate(n);
+    T* qq_old = allocate(n);
+    T* ql_old = allocate(n);
+
+    Domain* dptr = reinterpret_cast<Domain*>(sycl::malloc_shared(sizeof(Domain), oneapi::dpl::execution::dpcpp_default.queue()));
+    ::new(dptr) Domain();
 
     for (std::size_t i = 0; i < n_times; ++i) {
         std::vector<T> tmp(n);
 
         auto start = std::chrono::high_resolution_clock::now();
 
-        std::fill(std::execution::par_unseq, data_from1, data_from1 + n, 1);
+        std::for_each_n(std::execution::par_unseq, oneapi::dpl::counting_iterator<std::size_t>(0), n,
+                    [=](std::size_t i) {
+                      e_old[i] = dptr->e(i);
+                      delvc[i] = dptr->delv(i);
+                      p_old[i] = dptr->p(i);
+                      q_old[i] = dptr->q(i);
+                      qq_old[i] = dptr->qq(i);
+                      ql_old[i] = dptr->ql(i);
+                    });
 
         auto finish = std::chrono::high_resolution_clock::now();
         times.emplace_back(std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count());
@@ -43,13 +100,13 @@ int main() {
     std::sort(times.begin(), times.end());
     std::cout << "Elapsed time: " << times[n_times / 2] << " ms" << std::endl;
 
-    for (std::size_t i = 0; i < n; ++i) {
-        (data_from1 + i)->~T();
-        (data_from2 + i)->~T();
-        (data_to + i)->~T();
-    }
+    dptr->~Domain();
+    sycl::free(dptr, oneapi::dpl::execution::dpcpp_default.queue());
 
-    sycl::free(data_from1, oneapi::dpl::execution::dpcpp_default.queue());
-    sycl::free(data_from2, oneapi::dpl::execution::dpcpp_default.queue());
-    sycl::free(data_to, oneapi::dpl::execution::dpcpp_default.queue());
+    free(ql_old, n);
+    free(qq_old, n);
+    free(q_old, n);
+    free(p_old, n);
+    free(delvc, n);
+    free(e_old, n);
 }
