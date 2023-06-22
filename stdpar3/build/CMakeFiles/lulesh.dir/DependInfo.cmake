b4b52c06d40f5a7679450358edf2744e34462b6c
kboyarinov <konstantin.boyarinov@intel.com>
2023-06-22 12:02:38 +0300

    Add stdpar3 self port

---
+++ stdpar3/build/CMakeFiles/lulesh.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/git/kboyarinov/LULESH/stdpar3/lulesh-comm.cc" "CMakeFiles/lulesh.dir/lulesh-comm.cc.o" "gcc" "CMakeFiles/lulesh.dir/lulesh-comm.cc.o.d"
+  "/mnt/c/git/kboyarinov/LULESH/stdpar3/lulesh-init.cc" "CMakeFiles/lulesh.dir/lulesh-init.cc.o" "gcc" "CMakeFiles/lulesh.dir/lulesh-init.cc.o.d"
+  "/mnt/c/git/kboyarinov/LULESH/stdpar3/lulesh-util.cc" "CMakeFiles/lulesh.dir/lulesh-util.cc.o" "gcc" "CMakeFiles/lulesh.dir/lulesh-util.cc.o.d"
+  "/mnt/c/git/kboyarinov/LULESH/stdpar3/lulesh-viz.cc" "CMakeFiles/lulesh.dir/lulesh-viz.cc.o" "gcc" "CMakeFiles/lulesh.dir/lulesh-viz.cc.o.d"
+  "/mnt/c/git/kboyarinov/LULESH/stdpar3/lulesh.cc" "CMakeFiles/lulesh.dir/lulesh.cc.o" "gcc" "CMakeFiles/lulesh.dir/lulesh.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
